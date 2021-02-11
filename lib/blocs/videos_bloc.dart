18d5ee59856addfb70337986551cc8491b3f6c14
TomazSouza <developer.software.ti@gmail.com>
2021-02-11 17:19:57 -0300

    [add] lista vazia antes de carregar mais videos

--- lib/blocs/videos_bloc.dart
+++ lib/blocs/videos_bloc.dart
@@ -25,6 +25,7 @@ class VideosBloc implements BlocBase {
 
   void _search(String search) async {
     if (search != null) {
+      _videosController.sink.add([]);
       videos = await api.search(search);
     } else {
       videos += await api.nextPage();
